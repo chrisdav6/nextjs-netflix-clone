35686696ab48765e9c542f27f8cf99a19f3ba4ae
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-06-27 15:29:52 -0400

    Completed Header component

---
+++ components/Header.tsx
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+        console.log('Greater than 0');
+      } else {
+        setIsScrolled(false);
+        console.log('0');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className='flex items-center space-x-2 md:space-x-10'>
+        <img
+          src='/netflixLogo.svg'
+          width={100}
+          height={100}
+          alt='Netflix Logo'
+          className='cursor-pointer object-contain'
+        />
+
+        <ul className='hidden space-x-4 md:flex'>
+          <li className='headerLink'>Home</li>
+          <li className='headerLink'>TV Shows</li>
+          <li className='headerLink'>Movies</li>
+          <li className='headerLink'>New &amp; Popular</li>
+          <li className='headerLink'>My List</li>
+        </ul>
+      </div>
+
+      <div className='flex items-center space-x-4 text-sm font-light'>
+        <SearchIcon className='hidden sm:inline h-6 w-6' />
+        <p className='hidden lg:inline'>Kids</p>
+        <BellIcon className='h-6 w-6' />
+        <Link href='/account'>
+          <img
+            src='/netflixAvatar.png'
+            alt='Netflix Avatar'
+            className='cursor-pointer rounded'
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div>
       <Head>
         <title>Netflix Clone</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      {/* Header */}
+      <Header />
       <main>
         {/* Banner */}
 
